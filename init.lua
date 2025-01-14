42e3f3e10104a37fa90e433788f9186bf1520ce3
lemercie <lemercier@student.hive.fi>
2025-01-14 16:18:35 +0200

    add keaybind for LSP diagnostics

--- init.lua
+++ init.lua
@@ -510,6 +510,8 @@ require('lazy').setup({
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          map('<leader>e', vim.diagnostic.open_float, 'show [e]rror')
+
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
