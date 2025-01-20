e47c81e02ff1dbeca6475b0747a3f4da1905d7f4
lemercie <lemercier@student.hive.fi>
2025-01-20 14:45:57 +0200

    add plugin for toggling comments

--- init.lua
+++ init.lua
@@ -259,6 +259,11 @@ require('lazy').setup({
     },
   },
 
+  {
+    'numToStr/Comment.nvim',
+    opts = {},
+  },
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
